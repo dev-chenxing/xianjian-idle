b7bd3946547d5732e979f89778e6483da6a74f0a
陈刑 <dev-chenxing.io@outlook.com>
2025-02-14 17:57:01 +0800

    向东南西北走

---
+++ command/go.py
@@ -0,0 +1,15 @@
+from core.game import Game
+
+
+pattern = "向([东南西北上下])走"
+
+directions = ["东", "南", "西", "北", "上", "下"]
+
+def callback(arg: str):
+	from core.common import game
+	game: Game
+	李逍遥 = game.李逍遥
+	direction = arg
+	if direction in 李逍遥.room.exits:
+		# if 李逍遥.room.exit_blocked[direction]():
+			game.position_room(李逍遥, 李逍遥.room.exits[direction])
--- core/room.py
+++ core/room.py
@@ -3,20 +3,21 @@ from core.item_stack import ItemStack
 
 
 class Room:
-    def __init__(self, game, name: str, area: str | None):
-        if name in game.rooms:
-            raise ValueError("Already has a cell with the name "+name)
+	def __init__(self, game, name: str, area: str | None):
+		if name in game.rooms:
+			raise ValueError("Already has a cell with the name "+name)
 
-        self.area = area
-        self.name = name
-        self.full_name = f"{area}·{name}" if area else name
-        self.characters = []
-        self.items: list[ItemStack] = []
+		self.area = area
+		self.name = name
+		self.full_name = f"{area}·{name}" if area else name
+		self.characters = []
+		self.items: list[ItemStack] = []
+		self.exits: dict[str, Room] = {}
 
-        game.rooms[self.full_name] = self
+		game.rooms[self.full_name] = self
 
-    def describe(self):
-        print(f"\n$cyan$【{self.full_name}】")
-        if self.items:
-            for item_stack in self.items:
-                print(f"  {str(item_stack)}")
+	def describe(self):
+		print(f"\n$cyan$【{self.full_name}】")
+		if self.items:
+			for item_stack in self.items:
+				print(f"  {str(item_stack)}")
